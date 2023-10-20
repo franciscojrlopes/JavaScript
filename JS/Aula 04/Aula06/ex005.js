884e2c722451ec2c93578f43f37d69177de41c80
franciscojrlopes <fralopes@yahoo.com.br>
2023-10-20 20:00:53 -0300

    Update ex005

--- JS/Aula 04/Aula06/ex005.js
+++ JS/Aula 04/Aula06/ex005.js
@@ -8,3 +8,8 @@ console.log(`'Francisco' "José"`);// String = Texto
 
 console.log(1, 2, 3, 4, 5, 6) // Number = Número
 console.log(1 , 2, "Francisco José")
+
+//console.log('Meu nome é "Francisco". Estou aprendendo JavaScript as', 10, 'h da manhã!')
+
+console.log('Meu nome é "Francisco". Estou aprendendo JavaScript as, 10h da manhã.')
+console.log('Meu nome é "Francisco". Estou aprendendo JavaScript as,', 10,'h da manhã!')
