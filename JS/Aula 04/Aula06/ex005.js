f1a3d0a13e4a0eeaf1724a66af540d98e9fdc163
franciscojrlopes <fralopes@yahoo.com.br>
2023-10-20 19:59:27 -0300

    Update ex005

---
+++ JS/Aula 04/Aula06/ex005.js
@@ -0,0 +1,10 @@
+// função console log
+console.log('Francisco José');
+console.log("Francisco José");
+console.log(`Francisco José`);
+console.log("Francisco 'José'");
+console.log('"Francisco" José');
+console.log(`'Francisco' "José"`);// String = Texto
+
+console.log(1, 2, 3, 4, 5, 6) // Number = Número
+console.log(1 , 2, "Francisco José")
