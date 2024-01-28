f396fddb83448d06ea2fc5ebc29a48d0bd9a6569
franciscojrlopes <fralopes@yahoo.com.br>
2024-01-28 11:55:28 -0300

    Update aula5.js

--- JSH/aula5.js
+++ JSH/aula5.js
@@ -1,4 +1,4 @@
-let variavélCaixinha1 = 'Esse é o primeiro texto do arquivo!';
-const variavélCaixinha2 =  'Aqui se encontra o segundo arquivo!';
+let variavélCaixinha1 = "Esse é o primeiro texto do arquivo!";
+const variavélCaixinha2 = "Aqui se encontra o segundo arquivo!";
 console.log(variavélCaixinha1);
 console.log(variavélCaixinha2);
