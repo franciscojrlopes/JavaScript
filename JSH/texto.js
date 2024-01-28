5310498d32126df431a81dc8839b3178e5fa7e43
franciscojrlopes <fralopes@yahoo.com.br>
2024-01-28 11:55:33 -0300

    Update texto.js

--- JSH/texto.js
+++ JSH/texto.js
@@ -1,9 +1,9 @@
 const texto1 = "Olá meu nome é Francisco.";
-const texto2 =  "Eu Tenho 53 anos!"; 
+const texto2 = "Eu Tenho 53 anos!";
 const textoFinal = texto1 + texto2;
-console.log(textoFinal)
+console.log(textoFinal);
 
 const variavélNumérica1 = 27;
 const variavélNumérica2 = 35;
 resutadoFinal = variavélNumérica1 * variavélNumérica2;
-console.log(resutadoFinal)
+console.log(resutadoFinal);
