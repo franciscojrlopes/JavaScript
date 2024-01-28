b5911f1abeaad787fa13fafdf7e132e90b76f471
franciscojrlopes <fralopes@yahoo.com.br>
2024-01-27 21:01:04 -0300

    Update operadores.js

--- JSH/operadores.js
+++ JSH/operadores.js
@@ -1,6 +1,11 @@
-const resultado = 237  + 512;
-const resultado = 7 * 3;
+const resultadosoma = 237  + 512;
+const resultadomultiplicação = 7 * 3;
 const resultadocubo =  2**3;
+const resultadodivisão = 9/3;
+const restodado = 29%7;
 
-const restodadivisão = 29%7;
+let x = 27;
+x++
+console.log(x);
 
+const texto1 = 'Eu tenho 50 anos.';
