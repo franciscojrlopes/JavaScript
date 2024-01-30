6778c4e87e0231ed580cc2cce027ca6ec9e2adb5
franciscojrlopes <fralopes@yahoo.com.br>
2024-01-30 08:44:08 -0300

    Update condição_simples.js

--- JS/Aula 04/A09/condição_simples.js
+++ JS/Aula 04/A09/condição_simples.js
@@ -3,3 +3,11 @@ var vel = 160.5
 console.log(`A velocidade do seu carro é de ${vel} Km/h.`)
 console.log('Dirija sempre usando cinto de segurança!')
 
+var pais = 'Brasil';
+console.log(`Vivendo em ${pais}  `)
+    if (pais == 'Brasil') {
+        console.log('Você é Brasileiro!')
+
+    } else { console.log('Você é Estrangeiro!!!')
+
+        }
